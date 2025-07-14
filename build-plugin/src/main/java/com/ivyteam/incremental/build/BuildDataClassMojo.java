e97ee6563816ed5988be213044a2474b4e6ef884
Louis <louis.mueller@axonivy.com>
2025-07-14 15:02:08 +0200

    make incremental build work (#1)

--- build-plugin/src/main/java/com/ivyteam/incremental/build/BuildDataClassMojo.java
+++ build-plugin/src/main/java/com/ivyteam/incremental/build/BuildDataClassMojo.java
@@ -7,12 +7,13 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.Date;
 
+import javax.inject.Inject;
+
 import org.apache.commons.compress.utils.FileNameUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
@@ -21,8 +22,13 @@ import org.sonatype.plexus.build.incremental.BuildContext;
 @Mojo(name = "build-data-class", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class BuildDataClassMojo extends AbstractMojo {
 
-  @Component
   private BuildContext buildContext;
+  
+  @Inject
+  public BuildDataClassMojo(BuildContext buildContext) {
+    this.buildContext = buildContext;    
+  }
+
 
   @Parameter(property = "project", required = true, readonly = true)
   private MavenProject project;
--- sample/src/main/java/com/ivyteam/incremental/build/Main.java
+++
@@ -1,7 +0,0 @@
-package com.ivyteam.incremental.build;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
